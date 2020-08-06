3537a3516e816338f7a37b501a138f279acbe9bf
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-06 18:06:40 +0100

    red test for topping up a card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,9 @@ describe OysterCard do
   it 'has a balance of 0 upon initialization' do
     expect(subject.balance).to eq (0)
   end
+
+  it 'has the ability to top up a card' do
+    subject.top_up(5)
+    expect(subject.balance).to eq (5)
+  end
 end
