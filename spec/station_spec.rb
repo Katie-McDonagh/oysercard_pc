581be5579e6a08c9008f7d6944a7502b97cb88f0
Tim <TimCPB@github.com>
2020-08-07 11:54:23 +0100

    Station.rb passes 'has a zone on creation'

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,12 +2,12 @@ require 'station'
 
 describe Station do
   it 'has a name on creation' do
-    station = Station.new("Bank")
+    station = Station.new("Bank", 1)
     expect(station.name).to eq "Bank"
   end
 
-  # it 'has a zone on creation' do
-  #   station = Station.new("Bank", 1)
-  #   expect(station.zone).to eq 1
-  # end
+  it 'has a zone on creation' do
+    station = Station.new("Bank", 1)
+    expect(station.zone).to eq 1
+  end
 end
