43b26e0c55934513c156b7b3bcdb8d42add8a89f
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-07 12:29:53 +0100

    green test -starts with an empty entry/exit station

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,6 +3,14 @@ require 'journey'
 describe Journey do
 let(:station){ double :station }
 
+  it 'starts with an empty entry station' do
+    expect(subject.entry_station).to be(nil)
+  end
+
+  it 'starts with an empty exit station' do
+    expect(subject.exit_station).to be(nil)
+  end
+
   it 'sets an entry station' do
     subject.set_entry_station(station)
     expect(subject.entry_station).to eq station
