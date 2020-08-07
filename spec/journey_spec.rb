e72a6e1b75e4441ee52ab443c68e9104141564cc
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-07 12:34:55 +0100

    refactor tests

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,6 +2,8 @@ require 'journey'
 
 describe Journey do
 let(:station){ double :station }
+let(:entry_station) {double :station }
+let(:exit_station) { double :station }
 
   it 'starts with an empty entry station' do
     expect(subject.entry_station).to be(nil)
@@ -12,12 +14,12 @@ let(:station){ double :station }
   end
 
   it 'sets an entry station' do
-    subject.set_entry_station(station)
-    expect(subject.entry_station).to eq station
+    subject.set_entry_station(entry_station)
+    expect(subject.entry_station).to eq entry_station
   end 
 
   it 'sets an exit station' do
-    subject.set_exit_station(station)
-    expect(subject.exit_station).to eq station
+    subject.set_exit_station(exit_station)
+    expect(subject.exit_station).to eq exit_station
   end 
 end
