64a22e618485dbade26219961717a2036fbd1062
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-07 11:05:32 +0100

    refactor to make journey array

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,7 +8,7 @@ class OysterCard
 
   def initialize
     @balance = 0
-    @journeys = {}
+    @journeys = []
   end
 
   def top_up(number)
@@ -18,16 +18,16 @@ class OysterCard
 
   def touch_in(station)
     raise "minimum balance of #{OysterCard::MINBALANCE} required to touch in" if @balance < MINBALANCE
-    @journeys[:entry_station] = (station)
+    @journeys << {entry_station: station}
   end
 
   def touch_out(station)
     deduct(MINBALANCE)
-    @journeys[:exit_station] = (station)
+    @journeys.last.store(:exit_station, station)
   end
 
   def in_journey?
-    !!@journeys[:entry_station] && @journeys[:exit_station] == nil
+    @journeys.last.length == 1
   end
 
   private 
