c8d65c81c38a3877b002cb6f3e51ee158494ae55
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-07 16:26:06 +0100

    moving class responsability

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,16 @@
+require_relative 'journey'
 class OysterCard 
 
   MINBALANCE = 1
   MAXBALANCE = 90
   MINCHARGE = 1
 
-  attr_reader :balance, :journeys
+  attr_reader :balance, :journeys, :current_journey
 
-  def initialize
+  def initialize(current_journey = Journey.new)
     @balance = 0
     @journeys = []
+    @current_journey = current_journey
   end
 
   def top_up(number)
@@ -18,12 +20,15 @@ class OysterCard
 
   def touch_in(station)
     raise "minimum balance of #{OysterCard::MINBALANCE} required to touch in" if @balance < MINBALANCE
-    @journeys << {entry_station: station}
+    @current_journey.set_entry_station(station)
+    @journeys << {entry_station: @current_journey.entry_station}
   end
 
   def touch_out(station)
     deduct(MINBALANCE)
-    @journeys.last.store(:exit_station, station)
+    @current_journey.set_exit_station(station)
+    @journeys.push(@current_journey)
+    # @journeys.last.store(:exit_station, station)
   end
 
   def in_journey?
